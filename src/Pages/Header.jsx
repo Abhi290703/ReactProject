15c6879ceb82110aeb764abfe1b196759a02fc29
Abhi290703 <abhigec6@gmail.com>
2025-12-14 11:25:01 +0530

    add

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,6 +1,11 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import Toast from "./Toast";
+
 
 export default function Header() {
+  const [openToast, setOpenToast] = useState(false);
+
   const linkClass = ({ isActive }) =>
     isActive
       ? "text-indigo-600 font-semibold border-b-2 border-indigo-600 pb-1"
@@ -8,51 +13,33 @@ export default function Header() {
 
   return (
     <>
-    <header className="bg-black shadow-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+      <header className="bg-white shadow-md sticky top-0 z-50">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
 
-        {/* Logo */}
-        <h1 className="text-2xl font-bold text-indigo-600 cursor-pointer">
-          MyStore
-        </h1>
+          <h1 className="text-2xl font-bold text-indigo-600 cursor-pointer">
+            MyStore
+          </h1>
 
-        {/* Navigation Links */}
-        <nav>
+          {/* Navigation */}
           <ul className="flex gap-8 text-lg font-medium">
-            <li>
-              <NavLink to="/" className={linkClass}>
-                Home
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/products" className={linkClass}>
-                Products
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/blog" className={linkClass}>
-                Blog
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/userform" className={linkClass}>
-                UserForm  
-              </NavLink>
-            </li>
-
-             <li>
-              <NavLink to="/users" className={linkClass}>
-                Users  
-              </NavLink>
-            </li>
-
+            <li><NavLink to="/" className={linkClass}>Home</NavLink></li>
+            <li><NavLink to="/products" className={linkClass}>Products</NavLink></li>
+            <li><NavLink to="/blog" className={linkClass}>Blog</NavLink></li>
+            <li><NavLink to="/userform" className={linkClass}>UserForm</NavLink></li>
+            <li><NavLink to="/users" className={linkClass}>Users</NavLink></li>
           </ul>
-        </nav>
-      </div>
-    </header>
+
+          {/* Popup Button */}
+          <button
+            onClick={() => setOpenToast(true)}
+            className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-500"
+          >
+            Open Popup
+          </button>
+
+        </div>
+      </header>
+      <Toast open={openToast} onClose={() => setOpenToast(false)} />
     </>
   );
 }
