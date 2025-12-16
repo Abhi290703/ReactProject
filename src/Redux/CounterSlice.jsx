5cbae79704aae665cc1c5e0f9f1bfb280c150313
Abhi290703 <abhigec6@gmail.com>
2025-12-16 10:00:35 +0530

    add

---
+++ src/Redux/CounterSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const CounterSlice =createSlice( {
+        name : "Count",
+        initialState : 0,
+        reducers :{
+            INC:(State,action) => State+1,
+            DEC:(State,action) => State-1
+        }
+})
+
+export const {INC, DEC} = CounterSlice.actions;
+export default CounterSlice.reducer
