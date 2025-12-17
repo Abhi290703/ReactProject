e9f449b985e135c48170492c59cfe004785b93ae
Abhi290703 <abhigec6@gmail.com>
2025-12-17 21:59:29 +0530

    add

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from '../Redux/CounterSlice'
+import counterReducer from "./CounterSlice";
 
-export const Store = configureStore({
-    reducer:{
-        counter:counterReducer,
-    }
-})
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
