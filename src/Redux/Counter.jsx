bff3e14ac2836e4606c2dd7e61f343fdbe52698a
Abhi290703 <abhigec6@gmail.com>
2025-12-17 21:59:40 +0530

    add

--- src/Redux/Counter.jsx
+++ src/Redux/Counter.jsx
@@ -1,17 +1,29 @@
-import { useDispatch, useSelector } from "react-redux"
-import { DEC, INC } from "./CounterSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { INC, DEC } from "./CounterSlice";
 
-export const Counter = () =>{
+export const Counter = () => {
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
-    const count = useSelector((state)=>state.counter);
-    const dispatch = useDispatch();
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-2xl font-bold mb-4">Count: {count}</h1>
 
+      <div className="flex gap-4 justify-center">
+        <button
+          className="px-4 py-2 bg-green-600 text-white rounded"
+          onClick={() => dispatch(INC())}
+        >
+          INC
+        </button>
 
-    return(
-       <>
-       <h1>Count:{count}</h1>
-       <button onClick={()=>dispatch(INC())}>INC</button>
-       <button onClick={()=>dispatch(DEC())}>DEC</button>
-       </>
-    )
-}
+        <button
+          className="px-4 py-2 bg-red-600 text-white rounded"
+          onClick={() => dispatch(DEC())}
+        >
+          DEC
+        </button>
+      </div>
+    </div>
+  );
+};
