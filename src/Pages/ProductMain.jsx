ef6ffb3d52642322669e9b8d60b130eb898360c0
Abhi290703 <abhigec6@gmail.com>
2025-12-19 10:25:58 +0530

    add

---
+++ src/Pages/ProductMain.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductDetails from "../Products/ProductDetails";
+
+export const ProductMain = () => {
+  return (
+    <>
+      <ProductDetails />
+    </>
+  );
+};
