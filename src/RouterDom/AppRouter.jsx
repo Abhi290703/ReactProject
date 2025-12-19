3875627e51cd1a63be4d5f23b74e2a9062e14da6
Abhi290703 <abhigec6@gmail.com>
2025-12-19 10:26:38 +0530

    add

--- src/RouterDom/AppRouter.jsx
+++ src/RouterDom/AppRouter.jsx
@@ -1,31 +1,75 @@
+// import { Routes, Route } from "react-router-dom";
+// import Home from "../Pages/Home";
+// import Blog from "../pages/Blog";
+// import { UserForm } from "../Pages/UserForm";
+// import { Login } from "../Features/Login";
+// import { SignUp } from "../Features/Signup";
+// import Users from "../User/Users";
+// import Toast from "../pages/Toast";
+// // import Product from "../Products/product";
+// import DynamicRouting from "../Products/DynamicRouting";
+// import ProductDetails from "../Products/productDetails";
+// import { ProductMain } from "../Pages/ProductMain";
+
+
+
+// export default function AppRouter() {
+//   return (
+//     <>
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/productmain" element={<ProductMain/>} />
+//       <Route path="/product" element={<ProductDetails/>} />
+//       <Route path="/blog" element={<Blog/>} />
+//       <Route path="/userform" element={<UserForm/>} />
+//       <Route path="/signup" element={<SignUp/>} />
+//       <Route path="/login" element={<Login/>} />
+//       <Route path="/users" element={<Users/>} />
+//       <Route path="productDetails/:id" element={<DynamicRouting/>} />
+//       <Route path="/toast" element={<Toast/>}/>
+//     </Routes>
+//     </>
+//   );
+// }
+
+
+
+
+
+
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
-import Products from "../Pages/Products";
 import Blog from "../pages/Blog";
 import { UserForm } from "../Pages/UserForm";
 import { Login } from "../Features/Login";
 import { SignUp } from "../Features/Signup";
 import Users from "../User/Users";
-import DynamicRouting from "../dynamicRouting";
 import Toast from "../pages/Toast";
-
+import DynamicRouting from "../Products/DynamicRouting";
+import ProductDetails from "../Products/productDetails";
+import { ProductMain } from "../Pages/ProductMain";
+import Cart from "../Redux/cart/Cart";
 
 
 export default function AppRouter() {
   return (
-    <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/products" element={<Products/>} />
-      <Route path="/blog" element={<Blog/>} />
-      <Route path="/userform" element={<UserForm/>} />
-      <Route path="/signup" element={<SignUp/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/users" element={<Users/>} />
-      <Route path="products/:id" element={<DynamicRouting/>} />
-      <Route path="/toast" element={<Toast/>}/>
 
+      {/* Product Listing */}
+      <Route path="/products" element={<ProductDetails />} />
+
+      {/* Product Details */}
+      <Route path="/products/:id" element={<DynamicRouting />} />
+
+      <Route path="/productmain" element={<ProductMain />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/userform" element={<UserForm />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/toast" element={<Toast />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
-    </>
   );
 }
