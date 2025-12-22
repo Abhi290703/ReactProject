3453f0de03981950a11cd45f9ccfb0024d8ea230
Abhi290703 <abhigec6@gmail.com>
2025-12-22 12:04:50 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { Store } from "./Redux/Store.jsx";
+import Store from "./Redux/Store.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
