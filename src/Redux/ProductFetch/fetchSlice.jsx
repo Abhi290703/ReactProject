71b7d833d760ccbdc1f9409acb3f5c9d27b021ed
Abhi290703 <abhigec6@gmail.com>
2025-12-22 15:58:32 +0530

    add

---
+++ src/Redux/ProductFetch/fetchSlice.jsx
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const FetchSlice = createAsyncThunk(
+  "Products/Fetch",
+  async () => {
+    const { data } = await axios.get("https://dummyjson.com/products");
+    return data.products;
+  }
+);
+
+const fetchProducts = createSlice({
+  name: "ProductName",
+  initialState: {
+    products: [],
+    loading: false,
+    error: null
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(FetchSlice.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(FetchSlice.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(FetchSlice.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  }
+});
+
+export default fetchProducts.reducer;
